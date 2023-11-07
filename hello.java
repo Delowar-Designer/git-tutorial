c859210145e88efc35448710282df3f7ed045b6f
Delowar Hossan <delowar.designer@gmail.com>
2023-11-06 21:48:05 -0800

    addd anf jivp;rk

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greeting("Delowar.Designer");
+    }
+    puhlic static greeting(sting name) {
+        system.out.printin("Welcome": name);
     }
 }
